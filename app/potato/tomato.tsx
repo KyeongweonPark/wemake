86cb1e7a280da078fd94dea0afb5cb5626057294
semipia <semipia@semipiaui-MacBookAir.local>
2025-02-03 20:33:46 +0900

    initial

---
+++ app/potato/tomato.tsx
@@ -0,0 +1,10 @@
+export default function AboutUs() {
+  return <div>AboutUs</div>;
+}
+
+export const links = () => [{ rel: "stylesheet", href: "potatos.com" }];
+
+export const meta = () => [
+  { title: "Tomato" },
+  { name: "description", content: "Tomato" },
+];
